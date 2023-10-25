b344dee8a66859c9cb5b0710a62d0a62f639894b
Vivek Vats <vwvats@gmail.com>
2023-10-26 00:40:19 +0530

    navbar home link changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -43,7 +43,7 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-          <p className="text-white text-[18px] font-bold cursor-pointer">
+          <p className="ml-2 text-white text-[20px] font-medium cursor-pointer">
             Vivek Vats
           </p>
         </Link>
